8ced3269dea7e163c88ec6555f3c9e1afc9cc74b
Jakemessi <nanaveine@gmail.com>
2025-03-24 13:31:22 -0300

    Agora os valores das pérolas são limitados a 2 decimais

--- js/default.js
+++ js/default.js
@@ -3,8 +3,9 @@ var ultimovalor = 0
 function AtualizarValores(receba){
     valortotal = valortotal + receba
     ultimovalor = receba
-    document.getElementById("ultimovalor").innerHTML = `${ultimovalor}`
-    document.getElementById("valortotal").innerHTML = `${valortotal}`
+    document.getElementById("ultimovalor").innerHTML = `${ultimovalor.toFixed(2)}`
+    document.getElementById("valortotal").innerHTML = `${valortotal.toFixed(2)}`
+    document.getElementById("aviso").innerHTML = ``
 }
 
 function Somar(){
@@ -13,11 +14,11 @@ function Somar(){
     pesonabalanca = parseFloat(document.getElementById("pesonabalanca").value);
     resultado = precopormedida * (pesonabalanca / 10);
     if(Number.isNaN(precopormedida)){
-        document.getElementById("aviso").innerHTML = `O valor digitado no Preço por Medida é invalido, digite novamente.`
+        document.getElementById("aviso").innerHTML = `O valor digitado no Preço por Medida é invalido, digite novamente e troque vírgulas por ponto final.`
     }
     else{
         if(Number.isNaN(pesonabalanca)){
-            document.getElementById("aviso").innerHTML = `O valor digitado no Peso na Balança é invalido, digite novamente`
+            document.getElementById("aviso").innerHTML = `O valor digitado no Peso na Balança é invalido, digite novamente e troque vírgulas por ponto final`
         }
         else{
             document.getElementById("ultimovalor").innerHTML = `O resultado da soma é ${resultado}`
